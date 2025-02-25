3fc14f6b7a0c530f52dba08776b077cf3104a8d5
Tejas-2107 <tejaschougale2107@gmail.com>
2025-02-25 12:45:51 +0530

    implmented search api

--- elasticSearch.js
+++ elasticSearch.js
@@ -1,9 +1,50 @@
+const express = require("express");
+const router = express.Router();
 const { Client } = require('@elastic/elasticsearch-serverless');
+const dotenv = require("dotenv");
+dotenv.config();
+
 const client = new Client({
-    node: 'https://my-elasticsearch-project-e815b3.es.us-east-1.aws.elastic.cloud:443',
+    node: process.env.NODE,
     auth: {
         apiKey: process.env.ELASTIC_API_KEY
     }
 });
 
-module.exports = client;
+const indexName = 'practice';
+
+// Insert Single Document
+router.post('/add', async (req, res) => {
+    try {
+        const { name, branch, cgpa, roll_num } = req.body;
+        const response = await client.index({
+            index: indexName,
+            body: { name, branch, cgpa, roll_num },
+        });
+        res.json(response);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+//fetch data 
+router.get('/search', async (req, res) => {
+    try {
+        const result = await client.search({
+            index: indexName,
+            query: {
+                bool: {
+                    must: [
+                        { match_phrase: { "branch": "IT" } },
+                    ]
+                }
+            },
+            size: 10
+        });
+        res.json(result.hits.hits);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -2,25 +2,12 @@ const express = require("express");
 const client = require('./elasticSearch');
 const dotenv = require("dotenv");
 const app = express();
+const elastic = require("./elasticSearch")
 app.use(express.json());
 
 dotenv.config();
 
-const indexName = 'practice';
-
-// Insert Single Document
-app.post('/add', async (req, res) => {
-    try {
-        const { name, branch, cgpa, roll_num } = req.body;
-        const response = await client.index({
-            index: indexName,
-            body: { name, branch, cgpa, roll_num },
-        });
-        res.json(response);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+app.use("/", elastic);
 
 app.listen(5000, () => {
     console.log("Server Started on the port 5000");
